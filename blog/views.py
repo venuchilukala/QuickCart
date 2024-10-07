3c104fbdea4cb92d51e04d45e1c18a52c01ba56d
venuchilukala <venuchilukala07@gmail.com>
2024-10-07 22:09:29 +0530

    Added templates and static

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Blog App Index")
+    return render(request, 'blog/index.html')
--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('ShopHome')
+    return render(request, 'shop/index.html')
