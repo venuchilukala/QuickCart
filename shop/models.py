0ccea3902a1cdce6a86007e3ea50732f01adefdf
venuchilukala <venuchilukala07@gmail.com>
2024-10-09 11:50:47 +0530

    Added few endpoints & db reseted

--- shop/models.py
+++ shop/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 
 # Create your models here.
-
+      
 class Product(models.Model):
-    product_id = models.AutoField
+    product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
     category = models.CharField(max_length=50, default='')
     subcategory = models.CharField(max_length=50, default='')
     price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=300)
+    discount_price = models.IntegerField(default=0)  # New field for discount price
+    brand = models.CharField(max_length=50, default='')  # New field for brand
+    desc = models.TextField()
     pub_date = models.DateField()
     image = models.ImageField(upload_to='shop/images', default='')
-    
+
     def __str__(self):
         return self.product_name
+
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, unique=True, default='')  # Set email as unique
+    phone = models.CharField(max_length=70, default='')
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
 # Create your views here.
@@ -41,6 +41,14 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    if request.method == 'POST': 
+        name = request.POST.get('username', '')
+        email = request.POST.get('useremail', '')
+        phone = request.POST.get('number', '')
+        query = request.POST.get('query', '')
+        
+        contact = Contact(name=name, email=email, phone=phone, desc=query)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
