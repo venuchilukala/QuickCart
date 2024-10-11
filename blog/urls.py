57e1b6632d61457adf2b65abc650233fdbd78f68
venuchilukala <venuchilukala07@gmail.com>
2024-10-11 16:35:12 +0530

    created blog post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='BlogIndex')
+    path('', views.index, name='blogHome'),
+    path('blogpost/', views.blogpost, name='blogPost')
 ]
