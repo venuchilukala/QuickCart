500eb7d6642b5a084c6b2c4654d81887c25ceaa6
venuchilukala <venuchilukala07@gmail.com>
2024-10-11 17:17:01 +0530

    created post on blogpost

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blogpost
 
 # Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
 
-def blogpost(request):
-    return render(request, 'blog/blogpost.html')
+def blogpost(request, id):
+    #We are cathing id which is send from urlpatterns
+    # we [0] because to get 'About Myself' from <QuerySet [<Blogpost: About Myself>]>
+    # Then filtered post will be catched in blogpost.html
+    post = Blogpost.objects.filter(post_id = id)[0]
+    return render(request, 'blog/blogpost.html', {'post' : post})
